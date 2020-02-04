a3cbca7fc9f8720e35bbb26e3e14537fdc43f7b1
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-04 15:40:14 -0700

    npm install started on note.html

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -10,6 +10,11 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
