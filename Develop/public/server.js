18acf3276027b31faea4ff27b336ac1655904cd8
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-05 18:39:37 -0700

    fixed .css and index.js paths

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -14,11 +14,19 @@ app.use(express.static("public"));
 
 //HTML routes
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/assets/css/styles.css", function(req, res) {
+  res.sendFile(path.join(__dirname, "./assets/css/styles.css"));
+});
+
+app.get("/assets/js/index.js", function(req, res) {
+  res.sendFile(path.join(__dirname, "./assets/js/index.js"));
 });
 
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "notes.html"));
+  res.sendFile(path.join(__dirname, "./notes.html"));
 });
 
 //API routes
