9e2f0f269d0bc47c7557ef15350e6ef2f3ab613e
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-02-08 13:39:58 -0700

    Fixed relative paths to .html and .json

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post("/api/notes", function(req, res) {
   let newNote = req.body;
   // console.log("newNotes: ", newNote);
   //reads the existing db.json
-  fs.readFile("../db/db.json", "utf-8", (err, data) => {
+  fs.readFile("./Develop/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     //parses the data out of stringified
     let database = JSON.parse(data);
@@ -54,7 +54,7 @@ app.post("/api/notes", function(req, res) {
     }
     // console.log("Updated Database with post: ", database);
     //write the updated database var to db.json file
-    fs.writeFile("../db/db.json", JSON.stringify(database), function(err){
+    fs.writeFile("./Develop/db/db.json", JSON.stringify(database), function(err){
       if (err) throw err;
       //returns status and message as response
       return res.status(200).send("Note added");
@@ -64,7 +64,7 @@ app.post("/api/notes", function(req, res) {
 
 //  * DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
 app.delete("/api/notes/:id", function(req, res) {
-  fs.readFile("../db/db.json", "utf-8", (err, data) => {
+  fs.readFile("./Develop/db/db.json", "utf-8", (err, data) => {
     if (err) throw err;
     let database = JSON.parse(data);
     var paramId = parseInt(req.params.id);
@@ -74,7 +74,7 @@ app.delete("/api/notes/:id", function(req, res) {
     const updatedDatabase = database.filter(obj => obj.id !== paramId);
     // console.log("updatedDatabase: ", updatedDatabase);
 
-    fs.writeFile("../db/db.json", JSON.stringify(updatedDatabase), function(err){
+    fs.writeFile("./Develop/db/db.json", JSON.stringify(updatedDatabase), function(err){
       if (err) throw err;
       return res.status(200).send("Note deleted");
     })
